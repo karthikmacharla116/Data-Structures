81e7d5807dd84b2d45fc3ce6ae2b8eae2e468a27
Karthik <97028010+karthikmacharla116@users.noreply.github.com>
2024-01-14 09:31:54 +0530

    Create ClosedStrings.java

---
+++ ClosedStrings.java
@@ -0,0 +1,34 @@
+Problem Link: https://leetcode.com/problems/determine-if-two-strings-are-close/description/?envType=daily-question&envId=2024-01-14
+
+class ClosedStrings {
+    public boolean closeStrings(String word1, String word2) {
+        if (word1.length() != word2.length()) {
+            return false;
+        }
+
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+
+        // Count character frequencies in both strings
+        for (char c : word1.toCharArray()) {
+            freq1[c - 'a']++;
+        }
+
+        for (char c : word2.toCharArray()) {
+            freq2[c - 'a']++;
+        }
+
+        // Check if the set of frequencies is the same
+        for (int i = 0; i < 26; i++) {
+            if ((freq1[i] == 0 && freq2[i] != 0) || (freq1[i] != 0 && freq2[i] == 0)) {
+                return false;
+            }
+        }
+
+        // Sort frequency arrays and check if they are equal
+        Arrays.sort(freq1);
+        Arrays.sort(freq2);
+
+        return Arrays.equals(freq1, freq2);        
+    }
+}
