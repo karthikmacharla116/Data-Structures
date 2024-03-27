5489c050fd74edb63efb4b14f3b49eb0db3095dd
Karthik <97028010+karthikmacharla116@users.noreply.github.com>
2024-03-27 15:27:26 +0530

    Create SubArrayProdLessThanK.java

---
+++ SubArrayProdLessThanK.java
@@ -0,0 +1,19 @@
+Problem Link: https://leetcode.com/problems/subarray-product-less-than-k/?envType=daily-question&envId=2024-03-27
+
+class SubArrayProdLessThanK {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 1) return 0;
+
+        int left = 0, right = 0, product = 1, count = 0;
+        int n = nums.length;
+
+        while (right < n) {
+            product *= nums[right];
+            while (product >= k) product /= nums[left++];
+            count += 1 + (right - left);
+            right++;
+        }
+
+        return count;
+    }
+}
