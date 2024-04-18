b00df55345f79d6b97590d52ebf86d642f7931ca
Karthik <97028010+karthikmacharla116@users.noreply.github.com>
2024-04-18 18:03:35 +0530

    Create IslandPerimeter.java

---
+++ IslandPerimeter.java
@@ -0,0 +1,22 @@
+Problem Link:https://leetcode.com/problems/island-perimeter/
+
+public class IslandPerimeter {
+    public int islandPerimeter(int[][] grid) {
+        int perimeter = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1) {
+                    perimeter += 4;
+                    
+                    if (i > 0 && grid[i - 1][j] == 1) perimeter -= 2;
+                    if (j > 0 && grid[i][j - 1] == 1) perimeter -= 2;
+                }
+            }
+        }
+        
+        return perimeter;
+    }
+}
