1674e7c558ae84248f6eef9980a0297db74f7da1
Karthik <97028010+karthikmacharla116@users.noreply.github.com>
2024-04-25 17:47:16 +0530

    Create LongestIdealString.java

---
+++ LongestIdealString.java
@@ -0,0 +1,23 @@
+Problem Link: https://leetcode.com/problems/longest-ideal-subsequence/description/?envType=daily-question&envId=2024-04-25
+
+class LongestIdealString {
+    public int longestIdealString(String s, int k) {
+        int N = s.length();
+        int[] dp = new int[26];
+
+        int res = 0;
+        for (int i = 0; i < N; i++) {
+            int curr = s.charAt(i) - 'a';
+            int best = 0;
+            for (int prev = 0; prev < 26; prev++) {
+                if (Math.abs(prev - curr) <= k) {
+                    best = Math.max(best, dp[prev]);
+                }
+            }
+
+            dp[curr] = Math.max(dp[curr], best + 1);
+            res = Math.max(res, dp[curr]);
+        }
+        return res;
+    }
+}
