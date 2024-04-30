70ea4efc368fb7eab0b9c8db37c56e24a419ac20
Karthik <97028010+karthikmacharla116@users.noreply.github.com>
2024-04-30 12:53:55 +0530

    Create WonderfulSubStrings.java

---
+++ WonderfulSubStrings.java
@@ -0,0 +1,22 @@
+Problem Link: https://leetcode.com/problems/number-of-wonderful-substrings/description/?envType=daily-question&envId=2024-04-30
+
+class WonderfulSubStrings {
+    public long wonderfulSubstrings(String word) {
+        Map<Integer, Integer> count = new HashMap<>();
+        count.put(0, 1);
+        long result = 0;
+        int bitmask = 0;
+
+        for (char ch : word.toCharArray()) {
+            int charIndex = ch - 'a';
+            bitmask ^= 1 << charIndex;
+            result += count.getOrDefault(bitmask, 0);
+            for (int i = 0; i < 10; i++) {
+                result += count.getOrDefault(bitmask ^ (1 << i), 0);
+            }
+            count.put(bitmask, count.getOrDefault(bitmask, 0) + 1);
+        }
+
+        return result;
+    }
+}
