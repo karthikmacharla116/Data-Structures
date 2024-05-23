c93e181cf3df7d150d486736f55471840c27fc61
Karthik <97028010+karthikmacharla116@users.noreply.github.com>
2024-05-23 14:47:45 +0530

    Create BeautifulSubsets.java

---
+++ BeautifulSubsets.java
@@ -0,0 +1,27 @@
+Problem Link: https://leetcode.com/problems/the-number-of-beautiful-subsets/description/?envType=daily-question&envId=2024-05-23
+
+class BeautifulSubsets {
+    public int dfs(int[] nums, int idx, int k, HashMap<Integer, Integer> mp) {
+        if (idx == nums.length) return 1;
+
+        int taken = 0;
+        if (!mp.containsKey(nums[idx] - k) && !mp.containsKey(nums[idx] + k)) {
+            mp.put(nums[idx], mp.getOrDefault(nums[idx], 0) + 1);
+            taken = dfs(nums, idx + 1, k, mp);
+            mp.put(nums[idx], mp.get(nums[idx]) - 1);
+            if (mp.get(nums[idx]) == 0) {
+                mp.remove(nums[idx]);
+            }
+        }
+        
+        int notTaken = dfs(nums, idx + 1, k, mp);
+        
+        return taken + notTaken;
+    }
+
+    public int beautifulSubsets(int[] nums, int k) {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        int ans = dfs(nums, 0, k, mp);
+        return ans - 1;
+    }
+}
