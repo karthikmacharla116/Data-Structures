d0477079130ca8210cca170ca68c3d1ab152bf2c
Karthik <97028010+karthikmacharla116@users.noreply.github.com>
2024-06-21 11:54:05 +0530

    Create GrumptBookStoreOwner.java

---
+++ GrumptBookStoreOwner.java
@@ -0,0 +1,24 @@
+Problem Link: https://leetcode.com/problems/grumpy-bookstore-owner/description/?envType=daily-question&envId=2024-06-21
+
+class GrumptBookStoreOwner {
+    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+        int n = grumpy.length;
+        int i = 0;
+        int curWin = 0;
+        int maxWin = 0;
+
+        for(int j=0;j<n;j++) {
+            curWin+=customers[j] * grumpy[j];
+            maxWin = Math.max(curWin, maxWin);
+            if(j >= minutes - 1 ) {
+                curWin -= customers[i] * grumpy[i++];
+            }
+        }
+        int res = maxWin;
+        for(i=0;i<grumpy.length;i++) {
+            res+=customers[i] * (1 - grumpy[i]);
+        }
+        return res;
+
+    }
+}
